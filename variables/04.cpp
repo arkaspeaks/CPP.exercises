65d73e7e50498d793fcff742467accdaf188c58c
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-08 04:00:33 +0000

    cpp

---
+++ variables/04.cpp
@@ -0,0 +1,10 @@
+// Fill in the missing parts to create three variables of the same type, using a comma-separated list:
+# include <iostream>
+using namespace std;
+
+int main(){
+    int x= 5, y= 6, z= 50;
+    int sum = (x+y+z);
+    cout << sum << endl;
+    return 0;
+}
