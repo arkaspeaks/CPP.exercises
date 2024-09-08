44a85c6faab7bc1312eb50b6933e4a59f5c59023
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-08 03:43:19 +0000

    cpp

---
+++ syntax/01.cpp
@@ -0,0 +1,9 @@
+//Insert the missing part of the code below to output "Hello World!".
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Hello World !"<< endl;
+    return 0;
+}
