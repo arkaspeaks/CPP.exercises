73b1b1d55c88a9c56275a88c31e99d0c1622309b
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-08 04:26:03 +0000

    cpp

---
+++ DataTypes/01.cpp
@@ -0,0 +1,11 @@
+// Add the correct data type for the following variables:
+
+/*
+
+int myNum = 9;
+double myDoubleNum = 8.99;
+char myLetter = 'A';
+bool myBool = false;
+string myText = "Hello World";
+
+*/
---
+++ DataTypes/03.cpp
@@ -0,0 +1,10 @@
+// Create a greeting variable, and display the value of it:
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    string greeting = "hello";
+    cout << greeting << endl;
+    return 0;
+}
