e725bb1f05bfcdd11fa57addce59866c162475dd
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-09 04:15:26 +0000

    cpp

---
+++ Strings/06.cpp
@@ -0,0 +1,12 @@
+//Use the correct function to read a line of text which is put in by the user.
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    string fullName;
+    cout << "Type your full name: ";
+    getline(cin, fullName);
+    cout << "Your name is: " << fullName<< endl;
+    return 0;
+}
