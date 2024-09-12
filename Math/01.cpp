803dd51819636d0aee342951acfe52439226183a
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-12 03:54:19 +0000

    cpp

---
+++ Math/01.cpp
@@ -0,0 +1,13 @@
+//Use the correct function to print the highest value of a and b.
+
+#include <iostream>
+#include <cmath>
+
+using  namespace std;
+
+int main(){
+    int a = 5;
+    int b = 10;
+    cout<< max (a,b)<< endl;// this prints the highest value out of a and b.. here b 
+    return 0;
+}
---
+++ Math/02.cpp
@@ -0,0 +1,12 @@
+//Use the correct function to print the square root of x.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main (){
+    int a= 64;
+    int b = sqrt (a);
+    cout<< "the value of the square-root of a is " << b <<endl;
+    return 0;
+}
