3d7d7250b3bfeb8e9bd21175d585a2c9313a4ee3
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-12 04:02:41 +0000

    cpp

---
+++ Booleans/01.cpp
@@ -0,0 +1,12 @@
+//Fill in the missing parts to print the values 1 (for true) and 0 (for false):
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+    cout<<isCodingFun<<endl;
+    cout<< isFishTasty<<endl;
+    return 0;
+}
---
+++ Booleans/02.cpp
@@ -0,0 +1,11 @@
+// Fill in the missing parts to print the value true (for true):
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a = 10;
+    int b = 5;
+    cout << (a>b)<<endl; 
+    return 0;
+}
