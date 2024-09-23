fc26ed3063aeb33d8436409c583ff82449f5254d
Arka Banerjee <arkabanerjee381@gmail.com>
2024-09-23 08:02:48 +0000

    cpp

---
+++ loopsExercises/01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+//  Print Numbers from 1 to N
+// Write a program that takes an integer N as input and prints all numbers from 1 to N.
+
+
+int main(){
+    int a, N;
+    a = 1;
+    cout << "the value of N";
+    cin >> N;
+    while (a<N){
+        cout << a <<endl;
+        a++;
+    }
+return 0;
+}
